c5ee65aaeba40116133409bac295f0cfc96293ec
wenyu0416 <wenyu0416@163.com>
2021-10-10 18:17:24 +0800

    include sub doc in schema

--- index.js
+++ index.js
@@ -8,15 +8,37 @@ connect.then((db) => {
     console.log('connected correctly to server')
 
     Dishes.create({
-            name: 'wellry3',
+            name: 'wellry5',
             description: 'test for add via mongoose'
         })
         .then((dish) => {
-            console.log('dish ' + dish + ' saved successfully!');
+            return Dishes.findByIdAndUpdate(dish._id, {
+                    $set: { description: 'Updated test' }
+                }, {
+                    new: true
+                })
+                .exec();
+        })
+        .then((dish) => {
+            console.log('dish ' + dish + ' updated successfully!');
 
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then(() => {
             return Dishes.find({});
-        }).then((dishes) => {
-            console.log(dishes);
+        })
+        .then((dishes) => {
+            // console.log(dishes);
+            for (var i = 0; i < dishes.length; i++) {
+                console.log('dishes._doc[' + i + ']:' + JSON.stringify(dishes[i]._doc, null, 2));
+                // console.log('dishes._doc[' + i + '].comment[0]:' + JSON.stringify(dishes[i]._doc.comments[0]));
+            }
             return Dishes.remove({});
         }).then(() => {
             console.log('dishes removed');
