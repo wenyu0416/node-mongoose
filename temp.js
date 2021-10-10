636fe1f3914f95307feea2cd723bc38cc1bbf666
wenyu0416 <wenyu0416@163.com>
2021-10-10 17:47:29 +0800

    use schema create mthod instead of save()

--- temp.js
+++ temp.js
@@ -7,18 +7,8 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('connected correctly to server')
 
-    Dishes.create({
-            name: 'wellry3',
-            description: 'test for add via mongoose'
-        })
-        .then((dish) => {
-            console.log('dish ' + dish + ' saved successfully!');
-
-            return Dishes.find({});
-        }).then((dishes) => {
-            console.log(dishes);
-            return Dishes.remove({});
-        }).then(() => {
+    Dishes.remove({})
+        .then(() => {
             console.log('dishes removed');
             return mongoose.connection.close();
         }).catch((err) => {
